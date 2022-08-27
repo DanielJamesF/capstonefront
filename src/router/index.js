cc2683f7f13c9778bc6da268c95931edd2c4df4f
DanielJamesF <fredericksd17@gmail.com>
2022-08-27 14:02:27 +0200

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,56 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('../views/HomeView.vue')
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: () => import('../views/Admin.vue')
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: () => import('../views/Products.vue')
+  },
+  {
+    path: '/products/:id',
+    name: 'singleProducts',
+    component: () => import('../views/singleProduct.vue')
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: () => import('../views/Contact.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/Register.vue')
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    compnent: () => import('../views/Cart.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
