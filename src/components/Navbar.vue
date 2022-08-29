9acfc1511c09456f108fca615dc8d87af2ab582d
DanielJamesF <fredericksd17@gmail.com>
2022-08-29 16:14:40 +0200

    login

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,73 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav
+    class="navbar navbar-expand-md sticky-top"
+  >
+    <div class="container-fluid">
+      <div id="div" class="nav-item">
+        <router-link :to="{ name: 'home' }">
+        <img class="img-fluid" src="" alt="logo">
+        </router-link>
+      </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <div class="navbar-nav">
+          <div v-if="user" class="d-md-flex">
+            <router-link to="/">
+              <a class="nav-link">Home</a>
+            </router-link>
+            <router-link to="/products">
+              <a class="nav-link">Products</a>
+            </router-link>
+            <router-link to="/contact">
+              <a class="nav-link">Contact</a>
+            </router-link>
+            <router-link to="/cart">
+              <button class="btn rounded-pill">
+                <i class="fs-4 fa-solid fa-cart-shopping"></i>
+              </button>
+            </router-link>
+          </div>
+          <div v-if="admin" class="d-md-flex">
+            <router-link to="/admin">
+              <a class="nav-link">Admin</a>
+            </router-link>
+            <router-link to="/users">
+              <a class="nav-link">Users</a>
+            </router-link>
+          </div>
+          <div v-if="user">
+            <button class="btn" @click="logout">Logout</button>
+          </div>
+          <div v-else class="d-md-flex">
+            <router-link to="/login">
+              <a class="nav-link">Login</a>
+            </router-link>
+            <router-link to="/register">
+              <a class="nav-link">Register</a>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
   </nav>
 </template>
 
 <script>
 export default {
-name: 'Navbar'
-}
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+  },
+  computed: {
+    
+  }
+};
 </script>
 
 <style scoped>
